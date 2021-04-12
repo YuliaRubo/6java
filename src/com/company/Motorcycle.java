0b0fbe1e8571b1d398b732e8eb414484d1304ba6
Juli Ivance <ulya-rubo@mail.ru>
2021-04-12 21:54:33 +0300

    commit4

--- src/com/company/Motorcycle.java
+++ src/com/company/Motorcycle.java
@@ -1,15 +1,25 @@
 package com.company;
 
-public class  Motorcycle extends Vehicle {
+public class  Motorcycle extends Vehicle implements Costable{
     private int maxSpeed;
+    private int costInfo;
 
-    public Motorcycle(int wheelCount, int maxSpeed){
+
+    public Motorcycle(int wheelCount, int maxSpeed, int costInfo){
         super(wheelCount);
         this.maxSpeed=maxSpeed;
-    }
+        this.costInfo=costInfo;
+           }
+
     @Override
+    public  int getCost(){
+        return costInfo;
+    }
+
+
     public void printInfo(){
-        System.out.println("Транспортное средство: Мотоцикл - Количество колес: "+ wheelCount+ " ;" + " Максимальная скорость: " + maxSpeed);
+        System.out.println("Транспортное средство: Мотоцикл - Количество колес: "+ wheelCount+ " ;" + " Максимальная скорость: " + maxSpeed + ";"+"Стоимость :"+costInfo);
+
     }
 
 }
