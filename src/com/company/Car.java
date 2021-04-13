599b676bbd785a11452f4b0a8fdc95ad443b9128
Ulya Ivancova <ulya-rubo@mail.ru>
2021-04-13 09:01:06 +0200

    commit12

--- src/com/company/Car.java
+++ src/com/company/Car.java
@@ -1,17 +1,25 @@
 package com.company;
 
-public class Car extends  Vehicle {
+public class Car extends  Vehicle implements Costable{
     private int doorsCount;
+    private int costInfo;
 
 
 
-    public Car(int doorsCount, int wheelCount){
+    public Car(int doorsCount, int wheelCount, int costInfo){
         super( wheelCount);
         this.doorsCount = doorsCount;
+        this.costInfo = costInfo;
         }
+
+    @Override
+    public  int getCost(){
+        return costInfo;
+    }
     @Override
     public void printInfo(){
-        System.out.println("Транспортное средство: Машина - Количество дверей: "+ doorsCount+ "; " + "Количество колес: " + wheelCount);
+        System.out.println("Транспортное средство: Машина - Количество дверей: "+ doorsCount+ "; " +
+                "Количество колес: " + wheelCount+ ";"+ " Стоимость: " + costInfo);
 
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,16 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-    Car carOne = new Car(4, 4);
+    Car carOne = new Car(4, 4,240);
     carOne.printInfo();
-    Motorcycle motOne = new Motorcycle(2,250);
+    Motorcycle motOne = new Motorcycle(2,250, 400);
     motOne.printInfo();
 
-    Car1 carTwo = new Car1(250);
-        System.out.println("Стоимость машины:" + carTwo.printInfo());
 
-    Motorcycle1 motTwo = new Motorcycle1(350);
-        System.out.println("Стоимость мото:" + motTwo.printInfo());
 
 
 	// write your code here
