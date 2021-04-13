a8101bf4ddfb51457e6545a050c77368877c2a69
Ulya Ivancova <ulya-rubo@mail.ru>
2021-04-13 09:16:10 +0200

    commit13

--- src/com/company/Car.java
+++ src/com/company/Car.java
@@ -5,12 +5,12 @@ public class Car extends  Vehicle implements Costable {
     private int costInfo;
 
 
-
     public Car(int doorsCount, int wheelCount, int costInfo){
         super( wheelCount);
         this.doorsCount = doorsCount;
         this.costInfo=costInfo;
         }
+
     @Override
     public  int getCost(){
         return costInfo;
@@ -21,7 +21,6 @@ public class Car extends  Vehicle implements Costable {
         System.out.println("Транспортное средство: Машина - Количество дверей: "+ doorsCount+ "; " +
                 "Количество колес: " + wheelCount+ " ;" + "Стоимость:"+" " +
                  costInfo);
-
     }
 }
 
